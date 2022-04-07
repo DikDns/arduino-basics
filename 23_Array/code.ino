faf028e032f417d82156536f7f274340d4d4a142
Andika Eka Kurnia <73420758+DikDns@users.noreply.github.com>
2022-04-07 10:56:37 +0700

    Flow Control I 🟢

---
+++ 23_Array/code.ino
@@ -0,0 +1,18 @@
+
+const int timer = 100;
+const int leds[6] = {3,5,6,9,10,11};
+
+void setup(){
+  for (int i = 0; i < 6; i++){
+    pinMode(leds[i], OUTPUT); 
+  }
+}
+
+void loop(){
+  for(int i = 0; i < 6; i++){
+    digitalWrite(leds[i], HIGH);
+    delay(timer);
+    digitalWrite(leds[i], LOW);
+  }
+  
+}
